cc867eaee398656a29aab5c1de7d279ac31d6aab
Juyeong Lee <ju0123@ajou.ac.kr>
2026-02-08 19:24:42 +0900

    docs: move assets to docs

--- request.py
+++ request.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://localhost:8000/ingest"
 
-with open("2025_아주대학교_요람_소프트웨어학과.txt", "r", encoding="utf-8") as f:
+with open("docs/2025_아주대학교_요람_소프트웨어학과.txt", "r", encoding="utf-8") as f:
     text_data = f.read()
 
 payload = {
